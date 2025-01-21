9b1837c8f6ac9fe8bd3b72e1c6ec399bd6d1f893
ju3tin <ju3tin@hotmail.co.uk>
2025-01-21 11:24:33 +0000

    as

--- components/externalstyle.tsx
+++ components/externalstyle.tsx
@@ -4,14 +4,9 @@
 export function PreloadResources() {
   return (
     <link 
-      rel="preload"
+      rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@latest/css/all.min.css"
-      as="style"
       crossOrigin="anonymous"
-      type="text/css"
-      onLoad={() => {
-        console.log('Font Awesome CSS loaded')
-      }}
     />
   )
 }
