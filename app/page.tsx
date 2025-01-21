48ffe96449366a4eb0c07a1eb4f5dfc65ce6ea5a
ju3tin <ju3tin@hotmail.co.uk>
2025-01-21 11:02:39 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Nav from '../components/nav'
 import Footer from '../components/footer'
 import Video from '../components/video'
-
+import Selection from '../components/selection'
 export default function Page() {
   return (
     <main>
       <Nav />
       <Video />
+      <Selection />
       <Footer />
     </main>
   )
