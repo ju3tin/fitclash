e6abba6610b785e44fe7b79fb0daa07a7257bf6e
ju3tin <ju3tin@hotmail.co.uk>
2025-01-21 12:30:16 +0000

    update

---
+++ app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Nav from '../../components/nav'
+import Footer from '../../components/footer'
+import Video from '../../components/video'
+import Selection from '../../components/selection'
+import { PreloadResources } from '../../components/externalstyle'
+export default function Page() {
+  return (
+    <main>
+      <Nav />
+      <PreloadResources />
+      <Video />
+      <Selection />
+      <Footer />
+    </main>
+  )
+}
---
+++ components/masthead.tsx
@@ -0,0 +1,55 @@
+'use client';
+import Link from 'next/link';
+import React from 'react';
+
+const masthead = () => {
+  return (
+<div className="masthead">
+  <div className="masthead__inner-wrap">
+    <div className="masthead__menu">
+      <nav id="site-nav" className="greedy-nav">
+        
+        <a className="site-title" href="/">
+          <img src="/images/logo1.png" width="50vh" alt="Use your body as a controller"/>
+       
+        </a>
+        <ul className="visible-links"><li className="masthead__menu-item">
+              <a href="/games/">Games</a>
+            </li><li className="masthead__menu-item">
+              <a
+                href="/faq/"
+                
+                
+              >FAQ</a>
+            </li><li className="masthead__menu-item">
+              <a
+                href="/how-to-play/"
+                
+                
+              >How To Play</a>
+            </li><li className="masthead__menu-item">
+              <a
+                href="/app/"
+                
+                
+              >Launch App</a>
+            </li></ul>
+        
+       {/* <button class="search__toggle" type="button">
+          <span className="visually-hidden">Toggle search</span>
+          <i className="fas fa-search"></i>
+        </button>*/}
+        
+        <button className="greedy-nav__toggle hidden" type="button">
+          <span className="visually-hidden">Toggle menu</span>
+          <div className="navicon"></div>
+        </button>
+        <ul className="hidden-links hidden"></ul>
+      </nav>
+    </div>
+  </div>
+</div>
+);
+};
+
+export default masthead;
