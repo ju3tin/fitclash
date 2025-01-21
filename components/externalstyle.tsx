c2ce4bd58767d8499a38010698f4d52f824d95e4
ju3tin <ju3tin@hotmail.co.uk>
2025-01-21 11:52:01 +0000

    jj

--- components/externalstyle.tsx
+++ components/externalstyle.tsx
@@ -1,7 +1,16 @@
 // app/preload-resources.ts
 'use client'
+import { useEffect, useState } from 'react'
 
 export function PreloadResources() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return (
     <link 
       rel="stylesheet"
