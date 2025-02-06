f3b79d379efec12c6a8d5e4a30faa008ee21a7b5
ju3tin <ju3tin@hotmail.co.uk>
2025-02-06 12:05:57 +0000

    update

--- public/js/client1.js
+++ public/js/client1.js
@@ -714,6 +714,7 @@ function timer3(){
         if (lastState === "closed") {
             jumpCount++; // Increment jump count
             console.log("Star jump detected! Total jumps: " + jumpCount);
+            socket.emit('send_message', { roomId, player, jumpCount, }); 
         }
         lastState = "open"; // Update state to open
     } else {
