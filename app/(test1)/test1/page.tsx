7550744908faeff998f079b1b54d537003c6a227
ju3tin <ju3tin@hotmail.co.uk>
2025-02-25 10:25:01 +0000

    update

--- app/(test1)/test1/page.tsx
+++ app/(test1)/test1/page.tsx
@@ -1,7 +1,29 @@
+"use client"
 import Image from "next/image";
 import Footer from "../../../components/footer1";
+import { useEffect } from 'react';
 
 export default function Home() {
+  function gotopage() {
+    window.location.href = "/app/index.html";
+
+  }
+
+  useEffect(() => {
+    const element = document.querySelector(".border-gradient") as HTMLElement;
+
+    if (element) {
+      let angle = 0;
+      const rotateGradient = () => {
+        angle = (angle + 1) % 360; // Increment angle and keep it within 0-360
+        element.style.setProperty("--gradient-angle", `${angle}deg`);
+        requestAnimationFrame(rotateGradient); // Smooth animation loop
+      };
+
+      rotateGradient(); // Start the animation
+    }
+  }, []); // Empty dependency array to run once on mount
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -38,14 +60,18 @@ export default function Home() {
             />
             Play now
           </a>
-          <a
+        {/*   <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/app/index.html"
             target="_blank"
             rel="noopener noreferrer"
           >
             Launch App
-          </a>
+          </a>*/}
+          <button onClick={gotopage} className="border-gradient button5">
+          Launch App
+</button>
+
         </div>
       </main>
   <Footer />
