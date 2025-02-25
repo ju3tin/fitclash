bb24fac1dfa09885390e0b5da02293d08d48a0e5
ju3tin <ju3tin@hotmail.co.uk>
2025-02-25 09:18:55 +0000

    update

--- app/(test1)/test1/page.tsx
+++ app/(test1)/test1/page.tsx
@@ -38,11 +38,11 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://github.com/Danceclash"
+            href="/app/index.html"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Launch App
           </a>
         </div>
       </main>
