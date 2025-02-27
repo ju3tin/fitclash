fbed868a4487736b46a1258c9ea0ff77d739ce0e
ju3tin <ju3tin@hotmail.co.uk>
2025-02-27 14:00:51 +0000

    update

--- app/(game1)/app/page1.tsx
+++ app/(game1)/app/page1.tsx
@@ -26,14 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta property="og:image" content="https://danceclash.vercel.app/images/dance1.png" />
+      <meta property="og:image" content="https://fitclash.vercel.app/images/logo2.png" />
 <meta property="og:image:type" content="<generated>" />
 <meta property="og:image:width" content="<generated>" />
 <meta property="og:image:height" content="<generated>" />
 <meta property="og:site_name" content="https://danceclash.vercel.app" />
 <meta property="og:title" content="Dance Clash" />
 <meta property="og:description" content="Challange Your Friends To A Dance Battle" />
-<meta property="twitter:image" content="https://danceclash.vercel.app/images/dance1.png"></meta>
+<meta property="twitter:image" content="https://fitclash.vercel.app/images/logo2.png"></meta>
 <meta property="twitter:card" content="summary_large_image"></meta>
 <meta property="twitter:description" content="Challange Your Friends To A Dance Battle"></meta>
 <meta property="twitter:title" content="Dance Clash"></meta>
