3b3f0462646208718ae1e0dfebf0f5709b74146c
ju3tin <ju3tin@hotmail.co.uk>
2025-03-03 08:35:45 +0000

    update

--- components/header1.tsx
+++ components/header1.tsx
@@ -18,9 +18,7 @@ const handleClick = () => {
         <button onClick={() => setMenuOpen(!menuOpen)} className="text-white p-2">
           <Menu size={24} />
         </button>
-        <div className="flex-1 flex justify-center">
-          <Image src="/images/logo2.png" alt="Logo" width={40} height={40} />
-        </div>
+       
         <div>
         {/*  <Link href="/login"> */}
             <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
