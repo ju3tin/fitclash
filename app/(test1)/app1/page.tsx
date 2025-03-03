93eedc0418e2a2206a3da4ac849f9e4e45f8ab46
ju3tin <ju3tin@hotmail.co.uk>
2025-03-03 08:29:40 +0000

    update

--- app/(test1)/app1/page.tsx
+++ app/(test1)/app1/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Footer from "../../../components/footer1";
 import { useEffect } from 'react';
+import Header from "../../../components/header";
 
 export default function Home() {
   function gotopage() {
@@ -30,6 +31,8 @@ export default function Home() {
   }, []); // Empty dependency array to run once on mount
 
   return (
+    <>
+    <Header />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>
@@ -97,5 +100,6 @@ style={{cursor: 'pointer'}}
       </main>
   <Footer />
     </div>
+    </>
   );
 }
