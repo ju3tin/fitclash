bea14ae776b097573db37bd77123be005b16fea3
ju3tin <ju3tin@hotmail.co.uk>
2025-03-03 09:44:14 +0000

    updates

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,8 +14,8 @@ const Sidebar = () => {
         <nav className="p-4 space-y-4">
           <Link href="/" className="block py-2 px-4 hover:bg-gray-700 rounded">Home</Link>
           <Link href="/faq" className="block py-2 px-4 hover:bg-gray-700 rounded">Faq</Link>
-          <Link href="/services" className="block py-2 px-4 hover:bg-gray-700 rounded">Services</Link>
-          <Link href="/contact" className="block py-2 px-4 hover:bg-gray-700 rounded">Contact</Link>
+          <Link href="/games" className="block py-2 px-4 hover:bg-gray-700 rounded">Games</Link>
+          <Link href="/app1" className="block py-2 px-4 hover:bg-gray-700 rounded">Launch App</Link>
         </nav>
    </>
   
