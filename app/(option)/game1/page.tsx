e708f45d8454b1a8a0b5a26e8eb01902b6bccb77
ju3tin <ju3tin@hotmail.co.uk>
2025-03-04 09:50:23 +0000

    update

--- app/(option)/game1/page.tsx
+++ app/(option)/game1/page.tsx
@@ -82,7 +82,9 @@ export default function Home() {
   height={videoSize.height}
   style={{
     width: videoSize.width < videoSize.height ? `${videoSize.width}px` : "100%",
-    height: videoSize.width > videoSize.height ? "100%" :`${videoSize.height}px`  ,
+    height: videoSize.width > videoSize.height ? "100%" :`${videoSize.height}px`,
+    maxHeight: `${screen.height} !important`,
+    maxWidth: `${screen.width} !important`,
     border: "1px solid red",
   }}
 ></canvas>
