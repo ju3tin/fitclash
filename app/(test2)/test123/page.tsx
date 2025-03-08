22e135b6ba3aca21bf32e61b974f433582c94094
ju3tin <ju3tin@hotmail.co.uk>
2025-03-08 20:11:33 +0000

    update

--- app/(test2)/test123/page.tsx
+++ app/(test2)/test123/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [roomId, setroomId] = useState("");
 
   var socket: any;
-  socket = io("http://localhost:3001");
+  socket = io("https://webrtcsocket.onrender.com/");
 
   const handleJoin = () => {
     if (userName !== "" && roomId !== "") {
