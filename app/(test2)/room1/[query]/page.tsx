2219e654bffacabcd60363207234132795e3fbe0
ju3tin <ju3tin@hotmail.co.uk>
2025-03-09 15:06:52 +0000

    update

---
+++ app/(test2)/room1/[query]/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from "react";
+
+const Home = ({ children, href }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [tags, setTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    const currentParams = new URLSearchParams(searchParams.toString());
+    currentParams.set('tags', JSON.stringify(tags));
+    router.push(`?${currentParams.toString()}`);
+  }, [tags, searchParams, router]);
+
+  return (
+    <div>
+      Router: {JSON.stringify(searchParams.toString())}
+      <br />
+      Tags: {JSON.stringify(tags)}
+      <br />
+      <button onClick={() => setTags((prev) => [...prev, "foo"])}>
+        Add tag
+      </button>
+    </div>
+  );
+};
+
+export default Home; 
