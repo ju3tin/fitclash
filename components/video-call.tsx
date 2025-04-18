5ea137d02c40c320ee556d683831823e75feef0e
ju3tin <ju3tin@hotmail.co.uk>
2025-04-18 23:08:22 +0100

    Remove unused MongoDB import in VideoCall component and clean up TypeScript types

--- components/video-call.tsx
+++ components/video-call.tsx
@@ -26,6 +26,31 @@ type ConnectionStatus = {
   isConnected: boolean;
 };
 
+export const getServerSideProps: GetServerSideProps<
+  ConnectionStatus
+> = async () => {
+  try {
+    await client.connect();
+    // `await client.connect()` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
+};
+
 export default function VideoCall() {
 
   const [url, setUrl] = useState('');
