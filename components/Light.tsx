af6480de4e745cae588f90cb57044d8364f81b1f
ju3tin <ju3tin@hotmail.co.uk>
2025-04-21 02:36:39 +0100

    Comment out lighting components in Light.tsx and remove Model.tsx to clean up unused code and improve project maintainability.

--- components/Light.tsx
+++ components/Light.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Lights = () => {
   return (
     <>
-      <ambientLight intensity={0.1} />
+    {/*  <ambientLight intensity={0.1} />
       <directionalLight position={[40, 10, 5]} intensity={0.2} />
       <directionalLight
         castShadow
@@ -18,7 +18,7 @@ const Lights = () => {
         shadow-camera-bottom={-10}
       />
       <spotLight intensity={0.5} position={[90, 100, 50]} castShadow />
-    </>
+    */} </>
   );
 };
 
