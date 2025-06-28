c3e3f03a6988a60f815bad7ca1f3f224ad6bec14
ju3tin <ju3tin@hotmail.co.uk>
2025-06-28 22:22:41 +0100

    dfg

--- app/(test2)/videocall/page.tsx
+++ app/(test2)/videocall/page.tsx
@@ -1,10 +1,12 @@
 import "../globals.css";
 import { Suspense } from "react";
 import HomeContent from "../../../components/test123";
+import Header from "../../../components/header";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen p-4 md:p-8 bg-gray-50 max-w-6xl mx-auto">
+      <Header />
       <Suspense fallback={<div>Loading...</div>}>
         <HomeContent />
       </Suspense>
