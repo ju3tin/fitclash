c4b1deea9f551e0496f3abce6f8ab967e0e4a383
ju3tin <ju3tin@hotmail.co.uk>
2025-06-29 09:35:20 +0100

    dfg

--- app/(test2)/001/chat/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from 'react'
-
-export default function Layout ({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense>
-      <section>{children}</section>
-    </Suspense>
-  )
-}
